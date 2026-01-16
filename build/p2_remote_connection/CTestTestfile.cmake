35bfbf08910d154fdad9be9daba81fd42f7f0585
victor <z5482422@gmail.com>
2026-01-16 17:03:47 +1100

    Adding .gitignore for ros2 build artifacts

--- build/p2_remote_connection/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/unitree/p2_ws/P2RemoteConnection/src/p2_remote_connection
-# Build directory: /home/unitree/p2_ws/P2RemoteConnection/build/p2_remote_connection
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
